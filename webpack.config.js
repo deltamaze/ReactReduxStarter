b780ea2e85e619df10d1a8c97544db1f99b25265
deltamaze <wcpooley@gmail.com>
2019-06-13 23:25:46 -0500

    fixed webpack config and babel to work with later version

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,49 @@
+// module.exports = {
+//   entry: ['./src/index.js'],
+//   output: {
+//     path: __dirname,
+//     publicPath: '/',
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         exclude: /node_modules/,
+//         loader: 'babel',
+//         query: {
+//           presets: ['react', 'es2015', 'stage-1']
+//         }
+//       }
+//     ]
+//   },
+//   resolve: {
+//     extensions: ['', '.js', '.jsx']
+//   },
+//   devServer: {
+//     historyApiFallback: true,
+//     contentBase: './',
+//     watchOptions: {
+//       aggregateTimeout: 300,
+//       poll: 1000
+//     }
+//   }
+// };
+
 module.exports = {
-  entry: ['./src/index.js'],
   output: {
     path: __dirname,
     publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
+        use: {
+          loader: 'babel-loader'
         }
       }
     ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './',
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000
-    }
   }
 };
